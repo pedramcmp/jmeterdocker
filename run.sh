7640b867b156776e4d3c0cb6a1eebd5af7d9532f
pedramcmp <37592380+pedramcmp@users.noreply.github.com>
2019-04-24 10:54:31 +0200

    update run

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-jmeter -n -t /smoke.jmx -l /results.log -Jprot=${JPROT} -JIP=${JIP} -Jport=${JPORT}
+jmeter -n -t /testplan.jmx -l /results.log ${PARAMS}
 
-RESULT=$(grep false /results.log | wc -l)
 cat /results.log
+RESULT=$(grep -c false /results.log)
+echo -e "\nNumber of errors:" $RESULT "\n"
 if [ "$RESULT" = "0" ]; then
     exit 0
 else
